82de4eeea1f3c7434439042681715d582e8dca56
Lizeth-code389 <lizzce05@gmail.com>
2024-10-24 16:18:20 -0500

    Ejercicio de perfiles
    Se realizo un ejercicio de 3 imagenes con perfiles cada uno

---
+++ Frontend/src/views/main2.js
@@ -0,0 +1,13 @@
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      
+      arrayFrutas:['manzana','pera','fresa'],
+      info:[{"nombre":"Lizeth", "perfil":"Proframadora","img": "./imagen1.jpg","hobby":['Leer', 'pasear','ejercicio']},
+        {"nombre":"Esteban", "perfil":"Automatizador","img": "./imagen2.jpg","hobby":['videos','futball','pasear']},
+        {"nombre":"Rurito", "perfil":"Automatizador junior","img": "./imagen3.jpg" ,"hobby":['televición', 'pasear','comer']} ],
+
+    },
+})
+
