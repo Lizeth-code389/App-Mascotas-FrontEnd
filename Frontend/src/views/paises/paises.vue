17c67c0f5f088f56e0ad5797093f381e03dc5357
Lizeth-code389 <lizzce05@gmail.com>
2024-11-07 17:59:34 -0500

    Actualización de formulario
    Se realizo un formulario segun la clase del sena, donde se agrego una carpeta de Head y formulario

--- Frontend/src/views/paises/paises.vue
+++ Frontend/src/views/paises/paises.vue
@@ -1,30 +1,71 @@
-<script>
-//logica js
-
-import { ref } from 'vue'
-
-const nombre = ref('1110')
-
-
-</script>
-
-
-
 <template>
-
-   <h1>Modulos de paises</h1>
-
-   {{ nombre }}
-   <el-input v-model="nombre" style="width: 240px" placeholder="Ingrese su nombre" />
-
-
-   <el-button type="success">Success</el-button>
-
-
-
-</template>
-
-
-<style>
-/* estilos css*/
-</style>
+    <h1>Modulo de paises</h1>
+   <LayoutMain>
+     <template #slotLayout>
+       <Header 
+       :titulo="'Paises'"
+       :tituloBoton="'Crear Pais'"
+       :abrir="abrirFormulario"
+       />
+       
+       <Formulario :titulo="'Gestion de Paises'" v-model:is-open="mostrarFormulario" :is-edit="editandoFormulario"  >
+         <template #slotForm>
+           <el-row :gutter="20">
+             <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+           <formPaises   
+             v-model:is-open="mostrarFormulario" :is-edit="editandoFormulario"  
+           />
+         </el-col>
+       </el-row>
+         </template>
+ 
+       </Formulario>      
+ 
+           <el-table :data="tableData" stripe style="width: 100%">
+             <el-table-column prop="name" label="Nombre" width="180" />
+             <el-table-column prop="address" label="Direccion" width="180" />
+             <el-table-column prop="phone" label="Telefono" />
+             <el-table-column fixed="right" label="Acciones" min-width="120">
+               <template #default>
+                 <el-button link type="primary" size="large" :icon="Edit" @click="editarFormulario" >                  
+                 </el-button>
+                 <el-button link type="danger" :icon="Delete" ></el-button>
+               </template>
+             </el-table-column>
+           </el-table>
+          
+     </template>
+ 
+ 
+     </LayoutMain>
+   </template>
+     
+   <script lang="ts" setup>
+   import { reactive, ref } from 'vue'
+   import LayoutMain from '../../components/LayoutMain.vue';
+   import  formPaises from './components/formPaises.vue'
+   import Formulario from '../../components/Formulario.vue';
+   import Header from '../../components/Header.vue';
+   import  {Delete,Edit} from "@element-plus/icons-vue"
+ 
+   const mostrarFormulario = ref(false)
+   const editandoFormulario = ref(false)
+ 
+   const abrirFormulario =()=>{
+     mostrarFormulario.value=true
+     editandoFormulario.value=false
+   }
+ 
+   const editarFormulario= async()=>{
+     mostrarFormulario.value=true
+     editandoFormulario.value=true
+   }
+ 
+   const tableData = [
+   {
+     name: '2Oscar',
+     address: 'No. 189, Grove St, Los Angeles',
+     phone: '311555',
+   }
+ ]
+   </script>
