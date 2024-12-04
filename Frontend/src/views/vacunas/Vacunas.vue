9f908bbbc8740b2ddef8f1d174e0cfa29e12d38a
Lizeth-code389 <lizzce05@gmail.com>
2024-12-04 15:21:16 -0500

    Actualizar
    Se finalizo los formularios los cuales, tiene 5 formularios

--- Frontend/src/views/vacunas/Vacunas.vue
+++ Frontend/src/views/vacunas/Vacunas.vue
@@ -1,40 +1,39 @@
 <template>
 
-    <LayoutMain>
+<LayoutMain>
     <template #slotLayout>
       <Header 
       :titulo="'Vacunas'"
-      :tituloBoton="'Crear Vacunas'"
+      :tituloBoton="'Crear Vacuna'"
       :abrir="abrirFormulario"
       />
       
-
-      <Formulario :titulo="'Gestion de Vacunas'" v-model:is-open="mostrarFormulario" :is-edit="editandoFormulario"  >
+      <Formulario :titulo="'Gestion de Vacunas'" v-model:is-open="mostrarFormulario" :is-edit="editandoFormulario" @save="guardarDatos" @update="actualizarDatos" >
         <template #slotForm>
           <el-row :gutter="20">
             <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
-          <formVacunas 
-            v-model:is-open="mostrarFormulario" :is-edit="editandoFormulario"  
-          />
+          <formVacunas  
+          v-model:is-open="mostrarFormulario" :is-edit="editandoFormulario" ref="formRef"
+          :vacunas="vacunas" :dataValue="dataVacunasById"/>
         </el-col>
       </el-row>
         </template>
 
       </Formulario>      
 
-          <el-table :data="tableData" stripe style="width: 100%">
-            <el-table-column prop="nombre" label="Nombre" width="180" />
-            <el-table-column prop="descripción" label="Descripción" width="180"/>
-            <el-table-column prop="actualizar" label="Actualizar" width="180">
-
-              
-              <template #default>
-                <el-button link type="primary" size="large" :icon="Edit" @click="editarFormulario">                  
-                </el-button>
-                <el-button link type="danger" :icon="Delete" ></el-button>
-              </template>
-            </el-table-column>
-          </el-table>
+      <el-table :data="vacunas" stripe style="width: 100%">
+                <el-table-column prop="nombre" label="Nombre" />
+                <el-table-column prop="descripcion_vacuna" label="Descripcion_vacuna" />
+                <el-table-column fixed="right" label="Actualizar" min-width="120">
+                    <template #default="registro">
+                        <el-button link type="primary" size="large" :icon="Edit"
+                            @click="editarFormulario(registro.row.id)">
+                        </el-button>
+                        <el-button link type="danger" :icon="Delete"
+                            @click="eliminarVacuna(registro.row.id)"></el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
          
     </template>
 
@@ -44,31 +43,197 @@
     
   
   <script lang="ts" setup>
-  import { reactive, ref } from 'vue'
+  import { onMounted, reactive, ref, watch } from 'vue'
   import LayoutMain from '../../components/LayoutMain.vue';
-  import  formDepartamento from './componentes/formDepartamento.vue';
-  import formVacunas from './componentes/formVacunas.vue';
+  import  formVacunas from './componentes/formVacunas.vue'
   import Formulario from '../../components/Formulario.vue';
   import Header from '../../components/Header.vue';
   import  {Delete,Edit} from "@element-plus/icons-vue"
+  import { ElMessage, ElMessageBox } from 'element-plus'
+  import axios from 'axios';
 
-  const mostrarFormulario = ref(false)
-  const editandoFormulario = ref(false)
+  
+const mostrarFormulario = ref(false)
+const editandoFormulario = ref(false)
+const formRef = ref()
+const dataVacunasById = ref()
+const vacunas = ref([])
 
-  const abrirFormulario =()=>{
-    mostrarFormulario.value=true
-    editandoFormulario.value=false
-  }
 
-  const editarFormulario= async()=>{
-    mostrarFormulario.value=true
-    editandoFormulario.value=true
-  }
+const abrirFormulario = () => {
+    mostrarFormulario.value = true
+    editandoFormulario.value = false
+}
 
-  const tableData = [
-  {
-    codigo: '+57',
-    nombre: 'Colombia'
-  }
-]
-  </script>
+const editarFormulario = async (id) => {
+    dataVacunasById.value = await datosById(id)
+    mostrarFormulario.value = true
+    editandoFormulario.value = true
+}
+
+const guardarDatos = async () => {
+    const validacion = await formRef.value?.validarFormulario()
+    if (validacion) {
+        await crearVacunas()
+    }
+}
+
+const actualizarDatos = async () => {
+    const validacion = await formRef.value?.validarFormulario()
+    if (validacion) {
+        await actualizarVacuna()
+    }
+}
+
+const crearVacunas = async () => {
+
+    const url = 'http://127.0.0.1:8000/api/Vacuna/save'
+
+    const dataFormulario = {
+        codigo: formRef.value.formulario.codigo,
+        nombre: formRef.value.formulario.nombre
+        
+    }
+    try {
+        axios.post(url, dataFormulario)
+            .then(function (response) {
+                console.log(response);
+                formRef.value?.limpiarFormulario()
+                ElMessage({
+                    message: 'La vacuna se creo con exito    .',
+                    type: 'success',
+                })
+                datosVacuna()
+                mostrarFormulario.value = false
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+    } catch (error) {
+        console.error('error al crear vacuna ', error)
+    }
+}
+
+const actualizarVacuna = async () => {
+ 
+    const url = 'http://127.0.0.1:8000/api/Vacuna/update'
+
+    const dataFormulario = {
+        id:dataVacunasById.value[0].id,
+      
+        codigo: formRef.value.formulario.codigo,
+        nombre: formRef.value.formulario.nombre
+       
+    }
+    try {
+        axios.put(url, dataFormulario)
+            .then(function (response) {
+                console.log(response);
+                formRef.value?.limpiarFormulario()
+                ElMessage({
+                    message: 'Fue  actualizo exitosamente    .',
+                    type: 'success',
+                })
+                datosVacuna()
+                mostrarFormulario.value = false
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+    } catch (error) {
+        console.error('error al crear vacuna ', error)
+    }
+
+}
+
+const datosById = async (id) => {
+
+    const url = 'http://127.0.0.1:8000/api/Vacuna/datos?id=1'
+
+    try {
+        const response = axios.get(url, {
+            params: {
+                id: id
+            }
+        })
+        return (await response).data.result
+
+    } catch (error) {
+        console.error('error al crear vacuna ', error)
+    }
+
+}
+const eliminarVacuna = async (id) => {
+
+    const url = 'http://127.0.0.1:8000/api/Vacuna/delete'
+
+    ElMessageBox.confirm(
+        'Esta seguro de eliminar la vacuna ',
+        'Eliminar Registro',
+        {
+            confirmButtonText: 'SI',
+            cancelButtonText: 'Cancelar',
+            type: 'error',
+        }
+    )
+        .then(() => {
+
+            try {
+                axios.delete(url, { data: { id } })
+                    .then(function (response) {
+                        datosVacuna()
+
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+
+            } catch (error) {
+                console.error('error al crear vacuna ', error)
+            }
+            ElMessage({
+                type: 'success',
+                message: 'Se elimino correctamente el registro',
+            })
+        })
+        .catch(() => {
+            ElMessage({
+                type: 'info',
+                message: 'Eliminacion cancelada',
+            })
+        })
+
+}
+const datosVacuna = async () => {
+
+    const url = 'http://127.0.0.1:8000/api/Vacuna/datos'
+
+    try {
+        axios.get(url)
+            .then(function (response) {
+                vacunas.value = response.data.result
+               // console.log(response);
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+    } catch (error) {
+        console.error('error al crear vacuna ', error)
+    }
+
+
+}
+
+
+onMounted(() => {
+    /*getPais()*/
+    datosVacuna()
+})
+
+</script>
--- Frontend/src/views/vacunas/componentes/formVacunas.vue
+++ Frontend/src/views/vacunas/componentes/formVacunas.vue
@@ -1,18 +1,102 @@
 <template>
-    <el-card style="max-width: 100%">
-      <el-row>
-        <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
-            <h1>Formulario de Vacunas</h1>
-        </el-col>
-      </el-row>
-    </el-card>
-  </template>
+  <el-form ref="formRef" style="max-width: 100%" :model="formulario" :rules="rulesForm" label-width="auto"
+    :size="formSize" status-icon>
+    <el-form-item label="Codigo" prop="codigo">
+      <el-input v-model.number="formulario.codigo" />
+    </el-form-item>
+    
+    <el-form-item label="Nombre" prop="nombre">
+        <el-input v-model="formulario.nombre"  :label="Vacunas.nombre" />
+    </el-form-item>
 
+  </el-form>
+</template>
 
 <script setup>
+
+import { onMounted, reactive, ref, watch } from 'vue'
+import Vacunas from '../Vacunas.vue';
+
+const propiedad = defineProps({
+  Vacunas: {
+    type: Array,
+    required: true,
+  },
+  dataValue: Object,
+});
+
+const formSize = ref('default')
+const formRef = ref()
+const formulario = reactive({
+  codigo: '',
+  nombre: '',
+  
+
+})
+
+const datosFormulario = () => {
+
+  formulario.codigo = propiedad.dataValue[0].codigo;
+  formulario.nombre = propiedad.dataValue[0].nombre;
+  
+
+}
+
+const rulesForm = reactive({
+  codigo: [
+    { required: true, message: 'Por favor ingrese el codigo de la vacuna', trigger: 'blur' },
+  
+    {
+      type: 'number',
+      message: 'El código debe ser un número',
+      trigger: 'blur',
+    },
+    ],
+
+  nombre: [
+    {
+      required: true, 
+      message: 'Por favor ingrese el nombre de la vacuna',
+      trigger: 'blur',
+    },
+  ],
+  
+})
+
+const limpiarFormulario = () => {
+  formRef.value.resetFields()
+}
+
+const validarFormulario = () => {
+
+  return new Promise((resolve) => {
+    formRef.value?.validate((valid) => {
+      if (valid) {
+              resolve(true)
+            } else {
+                resolve(false)             
+            }
+            
+        })
+        })        
+
+    
+}
+
+watch(
+  () => propiedad.dataValue,
+  (newData) => {
+    datosFormulario();
+  }
+);
+
+defineExpose({validarFormulario,formulario,limpiarFormulario})
+
 
 
 </script>
 
-<style scoped>
-</style>
+
+
+
+<style scoped></style>
