07472713b1de66826e58b80b7a42239ec4e49ade
Lizeth-code389 <lizzce05@gmail.com>
2024-12-03 21:14:59 -0500

    Actualización
    Se actualizo la imagen y el formulario de pais

--- Frontend/src/views/departamento/componentes/formDepartamento.vue
+++ Frontend/src/views/departamento/componentes/formDepartamento.vue
@@ -1,18 +1,103 @@
 <template>
-    <el-card style="max-width: 100%">
-      <el-row>
-        <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
-            <h1>Formulario Departamento</h1>
-        </el-col>
-      </el-row>
-    </el-card>
-  </template>
+  <el-form ref="formRef" style="max-width: 100%" :model="formulario" :rules="rulesForm" label-width="auto"
+    :size="formSize" status-icon>
+    <el-form-item label="Codigo" prop="codigo">
+      <el-input v-model="formulario.codigo" />
+    </el-form-item>
+    <el-form-item label="Nombre" prop="nombre">
+      <el-input v-model="formulario.nombre" />
+    </el-form-item>
+    
 
+  </el-form>
+</template>
 
 <script setup>
+
+import { onMounted, reactive, ref, watch } from 'vue'
+
+const propiedad = defineProps({
+  departamento: {
+    type: Array,
+    required: true,
+  },
+  dataValue: Object,
+});
+
+const formSize = ref('default')
+const formRef = ref()
+const formulario = reactive({
+  codigo: '',
+  nombre: '',
+  
+
+})
+
+const datosFormulario = () => {
+
+  formulario.codigo = propiedad.dataValue[0].codigo;
+  formulario.nombre = propiedad.dataValue[0].nombre;
+  formulario.pais = propiedad.dataValue[0].id_pais;
+
+}
+
+const rulesForm = reactive({
+  nombre: [
+    { required: true, message: 'Por favor ingrese el nombre del departamento', trigger: 'blur' }
+  ],
+  codigo: [
+    {  
+      type: 'number',
+      required: true,
+      message: 'Ingrese el codigo del departamento',
+      trigger: 'blur',
+    },
+
+
+  ],
+  area: [
+    {
+      required: false,
+      message: 'Seleccione un valor',
+      trigger: 'blur',
+    },
+  ],
+})
+
+const limpiarFormulario = () => {
+  formRef.value.resetFields()
+}
+
+const validarFormulario = () => {
+
+  return new Promise((resolve) => {
+    formRef.value?.validate((valid) => {
+      if (valid) {
+              resolve(true)
+            } else {
+                resolve(false)             
+            }
+            
+        })
+        })        
+
+    
+}
+
+watch(
+  () => propiedad.dataValue,
+  (newData) => {
+    datosFormulario();
+  }
+);
+
+defineExpose({validarFormulario,formulario,limpiarFormulario})
+
 
 
 </script>
 
-<style scoped>
-</style>
+
+
+
+<style scoped></style>
--- Frontend/src/views/mascotas/componentes/formMascotas.vue
+++ Frontend/src/views/mascotas/componentes/formMascotas.vue
@@ -1,18 +1,161 @@
 <template>
-    <el-card style="max-width: 100%">
-      <el-row>
-        <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
-            <h1>Formulario de Mascotas</h1>
-        </el-col>
-      </el-row>
-    </el-card>
-  </template>
+  <el-form ref="formRef" style="max-width: 100%" :model="formulario" :rules="rulesForm" label-width="auto"
+    :size="formSize" status-icon>
+    <el-form-item label="Nombre" prop="nombre">
+      <el-input v-model="formulario.nombre" />
+    </el-form-item>
+    <el-form-item label="Edad" prop="edad">
+      <el-input v-model="formulario.edad" />
+    </el-form-item>
+  
+    <el-form-item label="Sexo" prop="sexo">
+      <el-input v-model="formulario.sexo" />
+    </el-form-item>
+  
+    <el-form-item label="Peso" prop="peso">
+      <el-input v-model="formulario.peso" />
+    </el-form-item>
+  
+    <el-form-item label="Esterilizado" prop="esterilizado">
+      <el-input v-model="formulario.esterilizado" />
+    </el-form-item>
+  
+    <el-form-item label="Descripción" prop="descripción">
+      <el-input v-model="formulario.descripcion" />
+    </el-form-item>
+    
 
+  </el-form>
+</template>
 
 <script setup>
+
+import { onMounted, reactive, ref, watch } from 'vue'
+
+const propiedad = defineProps({
+  mascotas: {
+    type: Array,
+    required: true,
+  },
+  dataValue: Object,
+});
+
+const formSize = ref('default')
+const formRef = ref()
+const formulario = reactive({
+  nombre: '',
+  edad: '',
+  sexo: '',
+  peso: '',
+  edad: '',
+  esterilizado: '',
+  descripcion: '',
+
+})
+
+const datosFormulario = () => {
+
+  formulario.nombre = propiedad.dataValue[0].nombre;
+  formulario.edad = propiedad.dataValue[0].edad;
+  formulario.sexo = propiedad.dataValue[0].sexo;
+  formulario.peso = propiedad.dataValue[0].peso;
+  formulario.esterilizado = propiedad.dataValue[0].esterilizado;
+  formulario.descripcion = propiedad.dataValue[0].descripcion;
+  formulario.raza = propiedad.dataValue[0].id_raza;
+
+}
+
+const rulesForm = reactive({
+  nombre: [
+    { required: true, message: 'Por favor ingrese el nombre de la mascota', trigger: 'blur' }
+  ],
+  edad: [
+    { 
+      type: 'number',
+      required: true,
+      message: 'Ingrese la edad de la mascota',
+      trigger: 'blur',
+    },
+
+  ],
+
+  sexo: [
+    {
+      required: true,
+      message: 'Ingrese el sexo de la mascota',
+      trigger: 'blur',
+    },
+  ],
+
+  peso: [
+    {  
+      type: 'number',
+      required: true,
+      message: 'Ingrese el peso',
+      trigger: 'blur',
+    },
+
+  ],
+
+  esterilizado: [
+    {
+      required: true,
+      message: 'Se encuentra esterilizado',
+      trigger: 'blur',
+    },
+  ],
+
+  descripcion: [
+    {
+      required: true,
+      message: 'Ingrese la descripcion de su mascota',
+      trigger: 'blur',
+    },
+  ],
+
+  /*area: [
+    {
+      required: false,
+      message: 'Seleccione un valor',
+      trigger: 'blur',
+    },
+  ],*/
+})
+
+const limpiarFormulario = () => {
+  formRef.value.resetFields()
+}
+
+const validarFormulario = () => {
+
+  return new Promise((resolve) => {
+    formRef.value?.validate((valid) => {
+      if (valid) {
+              resolve(true)
+            } else {
+                resolve(false)             
+            }
+            
+        })
+        })        
+
+    
+}
+
+watch(
+  () => propiedad.dataValue,
+  (newData) => {
+    datosFormulario();
+  }
+);
+
+defineExpose({validarFormulario,formulario,limpiarFormulario})
+
 
 
 </script>
 
-<style scoped>
-</style>
+
+
+
+<style scoped></style>
